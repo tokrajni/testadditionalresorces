ed08f6f7f981c35bb85fec9a96ff3bb4fda4df0a
Tomasz Okrajni <tomasz_okrajni@epam.com>
2022-02-02 09:17:40 +0100

    first commit

---
+++ src/main/java/com/example/testadditionalstaticresources/TestadditionalstaticresourcesApplication.java
@@ -0,0 +1,13 @@
+package com.example.testadditionalstaticresources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestadditionalstaticresourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestadditionalstaticresourcesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testadditionalstaticresources/TestadditionalstaticresourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testadditionalstaticresources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestadditionalstaticresourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
